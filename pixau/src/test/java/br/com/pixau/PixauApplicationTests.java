b24530f76beb67b3fca1038f62d4962b06356c43
Vinicius Lima <vinicius.slima99@icloud.com>
2023-12-14 15:31:25 -0300

    Criando o projeto Spring

---
+++ pixau/src/test/java/br/com/pixau/PixauApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pixau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
